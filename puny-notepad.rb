ad9cb6ed6f03635043b8bf7cf9564b0349b0b006
rrossilli55 <rrossilli55@gmail.com>
2022-11-28 12:21:44 -0500

    add formula

---
+++ puny-notepad.rb
@@ -0,0 +1,12 @@
+class PunyNotepad < Formula
+  desc ""
+  homepage ""
+  url "https://github.com/rrossilli/puny-notepad/archive/refs/tags/v1.0.0.tar.gz"
+  sha256 "e8ea09565bc73a67bad26c5458af5cb34d48e0b012f46afa27ae577c344a8144"
+  license ""
+
+  def install
+	  bin.install "puny-notepad"
+  end
+
+end
